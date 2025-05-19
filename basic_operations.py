f52383dd7be8160e1cf175c973efb3d48d3971c3
Mekdes D <mekdideriba3@gmail.com>
2025-05-19 18:46:19 +0300

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,5 +1,13 @@
+# Define the two variables
 number1 = 10
 number2 = 5
-print("addition of", number1 ,"and " ,number2 ,"is",(number1+number2))
-print("Subtraction of", number1 ,"and " ,number2 ,"is",(number1-number2))
-print("Multiplication of", number1 ,"and " ,number2 ,"is",(number1*number2))
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
